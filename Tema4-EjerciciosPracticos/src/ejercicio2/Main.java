ecc9a8f6653f32e04eabbec1ba5b72effb5515fb
jmgallego <jmgallego@106-09.info2>
2022-12-05 10:34:01 +0100

    Exercise 3 prepared

--- Tema4-EjerciciosPracticos/src/ejercicio2/Main.java
+++ Tema4-EjerciciosPracticos/src/ejercicio2/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		Esfera.radio = sc.nextDouble();
 		System.out.println();
 		
-		System.out.println("El radio de la esfera es " + Esfera.surface() + " y el volumen es " + Esfera.volume());
+		System.out.println("El radio de la esfera es " + Esfera.surface() + " u^2 y el volumen es " + Esfera.volume() + " u^3");
 		
 		sc.close();
 		
